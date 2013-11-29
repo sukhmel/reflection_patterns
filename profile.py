be3194093f308fdecd9357a6cb19487a12bd28d3
Vlad <sukhmel@users.noreply.github.com>
2013-11-29 12:55:04 +0300

    profile string usage example

--- profile.py
+++ profile.py
@@ -6,5 +6,5 @@ __author__ = 'sukhmel'
 import cProfile as profile
 from reflection import ReflectionPattern
 
-game = ReflectionPattern(profile = True, base = (997, 1499))
+game = ReflectionPattern(profile=True, profile_string="flood((100, 100),(255,0,0))", base=(816, 499), scale=(2, 2), pattern=[1,0])
 profile.run('game.execute()')
